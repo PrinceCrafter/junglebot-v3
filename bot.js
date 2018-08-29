17d7fb57117abef5d1ec41f1fda806b176a91c4e
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-08-29 14:13:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -203,6 +203,29 @@ if(cmd === `${prefix}sug`) {
 
 });
 
+  client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  let prefix = clientconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray.slice(1);
+
+if(cmd === `${prefix}server`);
+let sicon = message.guild.displyAvatarURL;
+let serverEmbed = new Discord.RichEmbed()
+.setDescription("``Server Information``")
+.addField("Server Name", message.guild.name)
+.addField("Created On", message.guild.createdAt)
+.addField("You Joined", message.member.joinedAt)
+.addField("Total Members", message.guild.memberCount)
+.addField("Server Owner", message.guild.owner.user.username)
+.addField("Rooms", message.guild.channels.size)
+.addField("Roles", message.guild.roles.size)
+.addField("Region", message.guild.region)
+.addField("Server ID", message.guild.id);
+
+return message.channel.send(serverEmbed);
+})
 
 client.on('message', message => {
      if(message.content.startsWith(prefix + "clear")) {
